3de08ffc5c9c29f7b69e291dccb87bd9b5454887
Maxim Bespalov <antibody@yandex.ru>
2018-06-22 19:01:09 +0300

    Limit burn and mint functions by time and amount

--- MintableTokenWithInitSupply_TimeAndEmitLimits.sol
+++ MintableTokenWithInitSupply_TimeAndEmitLimits.sol
@@ -4,13 +4,13 @@ import "./BasicToken.sol";
 import "./Ownable.sol";
 
  
-contract MintableTokenReducedWithBurnLimit2 is BasicToken, Ownable {
+contract MintableWithLimits is BasicToken {
   event Mint(address indexed to, uint256 amount);
-  event MintFinished();
+  
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Burn(address indexed from, uint256 value);
 
-  bool public mintingFinished = false;                         // see below finishMiniting function
+  
    
     string public name;
     string public symbol;
@@ -22,10 +22,23 @@ contract MintableTokenReducedWithBurnLimit2 is BasicToken, Ownable {
     uint256 internal burnIntervalTime;
     
     
-  modifier canMint() {                                         // see below finishMiniting function      
-    require(!mintingFinished);
-    _;
-  }
+  
+
+ /**
+  Not sure we need the following two functions for setting a moratorium on minitng process start
+   
+  
+  function getTime() public returns (uint256){                 // Returns current time. Check VISIBILITY 
+    return now;
+    }
+ 
+ 
+  function mintUnlocked() internal returns (bool) {           // Checks if current time is greater than set time that would be allowed for minint start 
+    return (getTime() >= 1525113448);                         // 04/30/2018 @ 1:21pm (UTC) in this case. Expresed as UNIX time
+    return (getTime() >= 1525215600);                       // 05/01/2018 @ 11:00pm (UTC). This should block miniting till the  evening of May 1st, if uncommented and upper line deleted
+    }
+    
+    */
 
   /**
    * Function to MINT new tokens
@@ -33,10 +46,10 @@ contract MintableTokenReducedWithBurnLimit2 is BasicToken, Ownable {
    *  _amount - The amount of tokens to mint.
    * return - a boolean that indicates if the operation was successful.
    */
-   
-  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
-    
-    require (_amount <= 1000);                        // limits a mint to 1000 tokens
+  function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
+      
+    //require (mintUnlocked());                   // prevents minitng untill time is competent for minting campaign start. Not sure if we need it
+    require (_amount <= 1000);                    // limits a mint to 1000 tokens
     require (mintTimer());                            // function that returns true in case intervalTime has elapsed (below)
    
     totalSupply_ = totalSupply_.add(_amount);
@@ -44,13 +57,14 @@ contract MintableTokenReducedWithBurnLimit2 is BasicToken, Ownable {
     emit Mint(_to, _amount);
     emit Transfer(address(0), _to, _amount);
     
-    mintIntervalTime = 60;                                // adds 60 sec to pause minting
+    mintIntervalTime = 60;                            // adds 60 sec to pause minting
     mintCurrentTime = now + mintIntervalTime;             // adds 60 sec to pause minting
-        
+    
+    
     return true;
     }
   
-function mintTimer() internal view returns (bool) {       // checks if mintIntervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
+function mintTimer() internal view returns (bool) {       // checks if intervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
      return (mintCurrentTime <= now);                      
     }
 
@@ -58,11 +72,7 @@ function mintTimer() internal view returns (bool) {       // checks if mintInter
    * @dev Function to stop minting new tokens.
    * @return True if the operation was successful.
    */
-  function finishMinting() onlyOwner canMint public returns (bool) {
-    mintingFinished = true;
-    emit MintFinished();
-    return true;
-    }
+  
   
  
     
@@ -83,7 +93,7 @@ function mintTimer() internal view returns (bool) {       // checks if mintInter
         
     }
     
-    function burnTimer() internal view returns (bool) {       // checks if burnIntervalTime has elapsed, if so returns "true", required for the burn function. Decide on VIEW vs PURE
+    function burnTimer() internal view returns (bool) {       // checks if intervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
      return (burnCurrentTime <= now);                      
     }
 
@@ -92,16 +102,17 @@ function mintTimer() internal view returns (bool) {       // checks if mintInter
    * This function allows to destroy (BURN) tokens 
       */
         function burn(uint256 _value) public returns (bool success) {
-        require(balances[owner] >= _value);          // Check if the sender has enough
+        require(balances[owner] >= _value);   // Check if the sender has enough
         require (_value <= 1000);                    // limits a burn to 1000 tokens
-        require (burnTimer());                       // function that returns true in case intervalTime has elapsed (below)
-        balances[owner] -= _value;                   // Subtract from the sender
+        require (burnTimer());                            // function that returns true in case intervalTime has elapsed (below)
+        balances[owner] -= _value;            // Subtract from the sender
         totalSupply_ -= _value;                      // Updates totalSupply
         emit Burn(msg.sender, _value);
         
-        burnIntervalTime = 60;                       // adds 60 sec to pause burning
-        burnCurrentTime = now + burnIntervalTime;    // adds 60 sec to pause burning
+        burnIntervalTime = 60;                            // adds 60 sec to pause burning
+        burnCurrentTime = now + burnIntervalTime;             // adds 60 sec to pause burning
         return true;
     }
 
    
+}
