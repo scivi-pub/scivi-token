c1412a9238997b4428ee52f724b4ffb502c606e9
Maxim Bespalov <antibody@yandex.ru>
2018-06-24 20:09:34 +0300

    Update MintableTokenWithInitSupply_TimeAndEmitLimits.sol

--- MintableTokenWithInitSupply_TimeAndEmitLimits.sol
+++ MintableTokenWithInitSupply_TimeAndEmitLimits.sol
@@ -13,7 +13,7 @@ contract MintableWithLimits is BasicToken {
    
     string public name;
     string public symbol;
-    uint8 public decimals = 0;
+    uint8 public decimals = 18;
     
     uint256 internal mintCurrentTime;
     uint256 internal burnCurrentTime;
@@ -48,28 +48,28 @@ contract MintableWithLimits is BasicToken {
   function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
       
     
-    require (_amount <= 1000);                    // limits a mint to 1000 tokens
-    require (mintTimer());                            // function that returns true in case intervalTime has elapsed (below)
+    require (_amount <= 500000);                             // limits a single mint to 500 000 tokens
+    require (mintTimer());                                   // function that returns true in case intervalTime has elapsed (below)
    
     totalSupply_ = totalSupply_.add(_amount);
     balances[_to] = balances[_to].add(_amount);
     emit Mint(_to, _amount);
     emit Transfer(address(0), _to, _amount);
     
-    mintIntervalTime = 60;                            // adds 60 sec to pause minting
-    mintCurrentTime = now + mintIntervalTime;             // adds 60 sec to pause minting
+    mintIntervalTime = 7885000;                                // equals to 3 months (91.3 days)
+    mintCurrentTime = now + mintIntervalTime;                  // adds 91.3 days to  to pause minting
     
     
     return true;
     }
   
-   function mintTimer() internal view returns (bool) {       // checks if intervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
+   function mintTimer() internal view returns (bool) {        // checks if intervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
      return (mintCurrentTime <= now);                      
     }
 
 
     
-   function burnTimer() internal view returns (bool) {       // checks if intervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
+   function burnTimer() internal view returns (bool) {        // checks if intervalTime has elapsed, if so returns "true", required for the mint function. Decide on VIEW vs PURE
      return (burnCurrentTime <= now);                      
     }
 
@@ -77,16 +77,16 @@ contract MintableWithLimits is BasicToken {
     /**
    * This function allows to destroy (BURN) tokens 
       */
-        function burn(uint256 _value) public returns (bool success) {
-        require(balances[owner] >= _value);   // Check if the sender has enough
-        require (_value <= 1000);                    // limits a burn to 1000 tokens
-        require (burnTimer());                            // function that returns true in case intervalTime has elapsed (below)
-        balances[owner] -= _value;            // Subtract from the sender
-        totalSupply_ -= _value;                      // Updates totalSupply
+        function burn(uint256 _value) onlyOwner public returns (bool success) {
+        require(balances[owner] >= _value);                  // Check if the sender has enough
+        require (_value <= 500000);                          // limits a burn to 500 000 tokens
+        require (burnTimer());                               // function that returns true in case intervalTime has elapsed (below)
+        balances[owner] -= _value;                           // Subtract from the sender
+        totalSupply_ -= _value;                              // Updates totalSupply
         emit Burn(msg.sender, _value);
         
-        burnIntervalTime = 60;                            // adds 60 sec to pause burning
-        burnCurrentTime = now + burnIntervalTime;             // adds 60 sec to pause burning
+        burnIntervalTime = 7885000;                               // equals to 3 months (91.3 days)
+        burnCurrentTime = now + burnIntervalTime;            // adds 91.3 days to  to pause burning
         return true;
     }
 
