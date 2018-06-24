9d3c6feff9eb00d18eb074f4b89d37e01e936929
Maxim Bespalov <antibody@yandex.ru>
2018-06-24 20:10:38 +0300

    removed onlyOwner modifier introduced earlier

--- BasicToken.sol
+++ BasicToken.sol
@@ -34,7 +34,7 @@ import "./Ownable.sol";
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  function transfer(address _to, uint256 _value) onlyOwner public returns (bool) {
+  function transfer(address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
 
